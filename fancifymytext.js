cae0eef5cd41822317496625644244e1f690329b
Jenica Chu <146392347+cixneja@users.noreply.github.com>
2024-12-04 01:46:34 -0800

    Bigger, Fancy, and Boring buttons now work

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,1 +1,28 @@
-alert("Hello, world!");
+function showAlert(){
+    alert("Hello, world!");
+}
+
+function getBigger(){
+    const textArea= document.getElementById("text");
+    textArea.style.fontSize = "24pt";
+}
+
+function makeFancy(){
+    const textArea = document.getElementById("text");
+    textArea.style.fontWeight="bold";
+    textArea.style.color="blue";
+    textArea.style.textDecoration="underline";
+}
+
+function makeBoring(){
+    const textArea = document.getElementById("text");
+    textArea.style.fontWeight="normal";
+    textArea.style.color="black";
+    textArea.style.textDecoration="none";
+}
+
+window.onload = function(){
+    document.getElementById("biggerButton").onclick = getBigger;
+    document.getElementById("fancifyOn").onchange = makeFancy;
+    document.getElementById("borifyOn").onchange = makeBoring;
+}
